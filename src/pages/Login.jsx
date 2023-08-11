371fd6f6f4348ba084c7c1928e8f9dc915764600
DarraghNolan <c17366881@mytudublin.ie>
2023-08-11 12:28:11 +0100

    trying to ge the login to work

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = ({ isLoggedIn, setIsLoggedIn }) => {
       // "http://localhost:8800/login"
 
       //  --  Heroku Connection
-      "https://incidentracker-e5eace51cecc.herokuapp.com/login"
+      "https://incidentracker-e5eace51cecc.herokuapp.com/users"
       , {
       Name: name,
       Password: password,
